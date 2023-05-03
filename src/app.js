7ecb92c04b92178fe9d8132e9ea722ceb572c0a4
Geo504 <129303891+Geo504@users.noreply.github.com>
2023-05-03 16:17:57 +0000

    Final version geo

--- src/app.js
+++ src/app.js
@@ -6,24 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 import { Button } from "bootstrap";
 
-window.onclick = function excuseGenerator() {
-  const who = ["The dog", "My grandma", "His turtle", "My bird"];
-  const action = ["ate", "peed", "crushed", "broke"];
-  const what = ["my homework", "the keys", "the car"];
-  const when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
+const who = ["The dog", "My grandma", "His turtle", "My bird"];
+const action = ["ate", "peed", "crushed", "broke"];
+const what = ["my homework", "the keys", "the car"];
+const when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-  const randomData = ar => {
-    return ar[Math.floor(Math.random() * ar.length)];
-  };
-
-  const myExcuse = document.getElementById("excuse");
-  myExcuse.innerHTML = `${randomData(who)} ${randomData(action)} ${randomData(
-    what
-  )} ${randomData(when)}`;
+const randomData = ar => {
+  return ar[Math.floor(Math.random() * ar.length)];
 };
+
+document.getElementById("btn").addEventListener("click", function() {
+  document.getElementById("excuse").innerHTML = `${randomData(
+    who
+  )} ${randomData(action)} ${randomData(what)} ${randomData(when)}`;
+  return document.getElementById("excuse").innerHTML;
+});
