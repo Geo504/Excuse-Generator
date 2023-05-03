cd02608b8c6a4f0ce3769795bf5a11ed1af9ec1b
Geo504 <129303891+Geo504@users.noreply.github.com>
2023-05-03 12:46:30 +0000

    First version geova

--- src/app.js
+++ src/app.js
@@ -4,8 +4,26 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { Button } from "bootstrap";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onclick = function excuseGenerator() {
+  const who = ["The dog", "My grandma", "His turtle", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "the keys", "the car"];
+  const when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const randomData = ar => {
+    return ar[Math.floor(Math.random() * ar.length)];
+  };
+
+  const myExcuse = document.getElementById("excuse");
+  myExcuse.innerHTML = `${randomData(who)} ${randomData(action)} ${randomData(
+    what
+  )} ${randomData(when)}`;
 };
